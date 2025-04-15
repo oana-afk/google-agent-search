dada780ca879733695410a2da6c370ebbbd3b7b1
juanvieira <juanvieira1@yahoo.com>
2025-04-15 14:33:03 -0300

    refactor: add new info

--- agents/code-reviewer/code_reviewer/shared_libraries/constants.py
+++ agents/code-reviewer/code_reviewer/shared_libraries/constants.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ROOT_AGENT_NAME = "code_reviewer"
-DESCRIPTION = "Review code from a especific repository"
+ROOT_AGENT_DESCRIPTION = "Review code from a especific repository"
+COMMITS_AGENT_NAME = "get_commit"
+COMMITS_AGENT_DESCRIPTION = "Get commit information from a specific repository"
+PR_AGENT_NAME = "get_pr"
+PR_AGENT_DESCRIPTION = "Get pull request information from a specific repository"
+
 PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT", "EMPTY")
 LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION", "global")
 MODEL = os.getenv("MODEL", "gemini-2.0-flash-001")
